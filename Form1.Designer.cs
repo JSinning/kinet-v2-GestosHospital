8aa8ca7c4da3772ea53451fcead173bacc72fe04
JUAN DAVID CASTAÑO SINNING <redesloclaes@yahoo.com>
2020-03-04 14:34:00 -0500

    Kinect camara a color y vgd de base de Datos de gestos

---
+++ Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(746, 484);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(1, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(384, 63);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "¿Que Sientes?";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(749, 489);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Kinect;
+using Microsoft.Kinect.VisualGestureBuilder;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        KinectSensor myKinect = null;
+        MultiSourceFrameReader reader = null;
+
+        VisualGestureBuilderDatabase vgbd;
+        VisualGestureBuilderFrameSource vgbfs;
+        VisualGestureBuilderFrameReader vgbr;
+
+        Gesture Gesture;        //Gesture oreja;
+        Gesture Gesture2;       //Gesto estomago
+
+        BodyFrameReader bfr;
+
+
+
+        public Form1()
+        {
+            myKinect = KinectSensor.GetDefault();
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           //Kinect = KinectSensor.GetDefault();
+            if(myKinect != null)
+            {
+                myKinect.Open();
+            }
+            reader = myKinect.OpenMultiSourceFrameReader(FrameSourceTypes.Color);
+            reader.MultiSourceFrameArrived += reader_MultiSourceFrameArrived;
+        }
+
+        private void reader_MultiSourceFrameArrived(object sender, MultiSourceFrameArrivedEventArgs e)
+        {
+            var reference = e.FrameReference.AcquireFrame();
+            using (var frame = reference.ColorFrameReference.AcquireFrame())
+            {
+                if(frame != null)
+                {
+                    var width = frame.FrameDescription.Width;   
+                    var hight = frame.FrameDescription.Height;
+                    var data = new byte[width * hight * 64 /16];
+                    frame.CopyConvertedFrameDataToArray(data, ColorImageFormat.Bgra);
+                    var bitmap = new Bitmap(width, hight, PixelFormat.Format32bppRgb);
+                    var bitmapData = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height),
+                        ImageLockMode.WriteOnly, bitmap.PixelFormat);
+                    Marshal.Copy(data, 0, bitmapData.Scan0, data.Length);
+                    bitmap.UnlockBits(bitmapData);
+                    pictureBox1.Image = bitmap;
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            bfr = myKinect.BodyFrameSource.OpenReader();
+            bfr.FrameArrived += bfr_FrameArrived;
+
+            vgbd = new VisualGestureBuilderDatabase("C:/Users/INGENIERO POL/Documents/Poryectos visual/WindowsFormsApp1/WindowsFormsApp1/DB/D_cabeza.gba");
+            vgbfs = new VisualGestureBuilderFrameSource(KinectSensor.GetDefault(), 0);
+
+            foreach (Gesture g in vgbd.AvailableGestures)
+            {
+           
+                if (g.Name.Equals("D_cabeza"))
+                {
+                    Gesture = g;
+                    vgbfs.AddGesture(Gesture);
+                }
+                if (g.Name.Equals("D_garganta"))
+                {
+                    Gesture2 = g;
+                    vgbfs.AddGesture(Gesture2);
+                }
+
+             }
+            vgbr = vgbfs.OpenReader();
+            vgbfs.GetIsEnabled(Gesture);
+            vgbfs.GetIsEnabled(Gesture2);
+            vgbr.FrameArrived += vgbr_FrameArrived;
+            myKinect.Open();
+
+        }
+
+        private void vgbr_FrameArrived(object sender, VisualGestureBuilderFrameArrivedEventArgs e)
+        {
+            using (var frame = e.FrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    if (vgbfs.IsTrackingIdValid)
+                    {
+                        IReadOnlyDictionary<Gesture, DiscreteGestureResult> discreteResult = frame.DiscreteGestureResults;
+                        if (discreteResult != null)
+                        {
+                            foreach (Gesture gesture in this.vgbfs.Gestures)
+                            {
+                                if (gesture.Name.Equals(" D_cabeza") && gesture.GestureType == GestureType.Discrete)
+                                {
+                                    DiscreteGestureResult result = null;
+                                    discreteResult.TryGetValue(gesture, out result);
+                                    if (result != null)
+                                    {
+                                        if (result.Detected && result.FirstFrameDetected)
+                                        {
+                                            // Console.WriteLine("Cabeza");
+                                            label1.Text = " D_cabeza";
+                                        }
+                                    }
+                                }
+                                else if (gesture.Name.Equals("D_garganta") && gesture.GestureType == GestureType.Discrete)
+                                {
+                                    DiscreteGestureResult result = null;
+                                    discreteResult.TryGetValue(gesture, out result);
+                                    if (result != null)
+                                    {
+                                        if (result.Detected && result.FirstFrameDetected)
+                                        {
+                                            //Console.WriteLine("Oreja");
+                                            label1.Text = "D_garganta";
+                                        }
+                                    }
+                                }
+
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void bfr_FrameArrived(object sender, BodyFrameArrivedEventArgs e)
+        {
+            if (!vgbfs.IsTrackingIdValid)
+            {
+                using (BodyFrame bodyFrame = e.FrameReference.AcquireFrame())
+                {
+                    if (bodyFrame != null)
+                    {
+                        Body[] bodies = new Body[6];
+                        bodyFrame.GetAndRefreshBodyData(bodies);
+                        Body bodyclose = null;
+
+                        foreach (Body b in bodies)
+                        {
+                            if (b.IsTracked)
+                            {
+                                if (bodyclose == null)
+                                {
+                                    bodyclose = b;
+                                }
+                                else
+                                {
+                                    Joint newhead = b.Joints[JointType.HandRight];
+                                    Joint oldHead = bodyclose.Joints[JointType.HandRight];
+                                    if (newhead.TrackingState == TrackingState.Tracked && newhead.Position.Z < oldHead.Position.Z)
+                                    {
+                                        bodyclose = b;
+                                        Console.WriteLine(bodyclose);
+                                    }
+                                }
+                            }
+
+                        }
+                        if (bodyclose != null)
+                        {
+                            vgbfs.TrackingId = bodyclose.TrackingId;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
